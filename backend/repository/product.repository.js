3de0573825f0bc8333b39d535973784a36b7e350
Adelquis1995 <adelquis1995@gmail.com>
2026-07-05 21:24:08 -0300

    agrego obtener y editar producto por id

--- backend/repository/product.repository.js
+++ backend/repository/product.repository.js
@@ -1,21 +1,42 @@
 import Product from "../models/product.model.js";
 
-class ProductRepository{
-    async getAllProducts(){
-        const products = await Product.find({active: true}).sort({createdAt: -1, updatedAt: -1}).populate({
-            path: 'category',
-            match: {active: true},
-            select: 'name'
-        })
+class ProductRepository {
+  async getAllProducts() {
+    const products = await Product.find({ active: true })
+      .sort({ createdAt: -1, updatedAt: -1 })
+      .populate({
+        path: "category",
+        match: { active: true },
+        select: "name",
+      });
 
-        return products.filter(p => p.category !== null)
-    }
+    return products.filter((p) => p.category !== null);
+  }
 
-    async deleteProductById(id){
-        const product = await Product.findByIdAndUpdate(id, {active: false}, {new: true})
-        return product
-    }
+  async deleteProductById(id) {
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { active: false },
+      { new: true },
+    );
+    return product;
+  }
+
+  async getProductById(id) {
+    const product = await Product.findOne({ _id: id, active: true }).populate({
+      path: "category",
+      match: { active: true },
+      select: "name",
+    });
+
+    return product;
+  }
+
+  async updateProductById(id, data) {
+    const product = await Product.findByIdAndUpdate(id, data, { new: true });
+    return product;
+  }
 }
 
-const productRepository = new ProductRepository()
-export default productRepository
+const productRepository = new ProductRepository();
+export default productRepository;
